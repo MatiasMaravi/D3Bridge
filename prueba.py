c2e5a4b11968f7e772db30a92e15ab19c6724bb3
MATIUS <matias.maravi@utec.edu.pe>
2026-04-03 14:33:23 -0500

    fix: changed name

--- prueba.py
+++
@@ -1,9 +0,0 @@
-import xgboost
-import shap
-# train XGBoost model
-X, y = shap.datasets.adult()
-model = xgboost.XGBClassifier(n_estimators=100, max_depth=2).fit(X, y)
-
-# compute SHAP values
-explainer = shap.Explainer(model, X)
-shap_values = explainer(X[:1000])
